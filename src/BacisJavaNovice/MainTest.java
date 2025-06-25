0959b02002a34e385b4b4e7aea9dcffa60151b5d
Yuliia Bezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-06-25 21:39:05 +0200

    add Junit Maven Libraries add TestClass empty

---
+++ src/BacisJavaNovice/MainTest.java
@@ -0,0 +1,21 @@
+package BacisJavaNovice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void myName() {
+
+    }
+
+    @Test
+    void isMehrAls0() {
+    }
+
+    @Test
+    void square() {
+    }
+}
