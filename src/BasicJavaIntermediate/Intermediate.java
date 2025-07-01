247ed500efbf797a42873adfdceeeb2f16feeb74
Yuliia Bezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-07-01 15:23:25 +0200

    add new Task Method array 100

--- src/BasicJavaIntermediate/Intermediate.java
+++ src/BasicJavaIntermediate/Intermediate.java
@@ -5,4 +5,26 @@ public class Intermediate {
         String [] arrayNummers = {"eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn"};
          return arrayNummers[nummer - 1];
     }
+
+    public static void main(String[] args) {
+        makeArray100_3_5();
+    }
+    public static void makeArray100_3_5(){
+        String[] array100 = new String[100];
+        int counter = 1;
+        for(int i = 0; i < 100; i++){
+            if (counter % 3 == 0 && counter % 5 == 0){
+                array100[i] = "#$";
+            } else if (counter % 3 == 0){
+                array100[i] = "#";
+            } else if(counter % 5 == 0) {
+                array100[i] = "$";
+            } else {
+                array100[i] = "" + counter;
+            }
+            System.out.print(array100[i] + "\t");
+            counter++;
+        }
+
+    }
 }
